a7fcd112d28bd865bfe4a6a0b0432c7e976bcc38
Z62528229 <142338509+Z62528229@users.noreply.github.com>
2023-08-19 10:24:58 +0000

    edited header

---
+++ workflow_train_model.py
@@ -0,0 +1,11 @@
+
+# Train the recommender model using a Gradient Workflow
+#
+# This script is minimally commented because it is called from and explained by
+# the recommender project notebook, deep_learning_recommender_tf.ipynb
+#
+# Code is the minimal subset of the notebook to perform the steps
+#
+# Lines are the same as the notebook, except where they need to be changed to
+# work in a .py script as opposed to a .ipynb notebook
+#
