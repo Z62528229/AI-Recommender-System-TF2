c9cac14c86c83e24d5c6e53408e07f50f63f09a1
Z62528229 <142338509+Z62528229@users.noreply.github.com>
2023-08-22 12:42:47 +0000

    Fix sync

--- workflow_train_model.py
+++ workflow_train_model.py
@@ -9,3 +9,12 @@
 # Lines are the same as the notebook, except where they need to be changed to
 # work in a .py script as opposed to a .ipynb notebook
 #
+# Last updated: Aug 03rd 2021
+
+# Setup
+
+import subprocess
+
+subprocess.run('pip install --upgrade pip', shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+subprocess.run('pip install -q tensorflow-recommenders==0.4.0', shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+subprocess.run('pip install -q --upgrade tensorflow-datasets==4.2.0', shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
