efa32cc39661c67a0634d622580082cf6bc347c3
Z62528229 <142338509+Z62528229@users.noreply.github.com>
2023-09-15 08:21:13 +0000

    Fixed some minor issues.

--- workflow_train_model.py
+++ workflow_train_model.py
@@ -99,3 +99,18 @@ class MovielensModelTunedRanking(tfrs.models.Model):
     def __init__(self) -> None:
         super().__init__()
         embedding_dimension = 32
+
+        self.movie_model: tf.keras.layers.Layer = tf.keras.Sequential([
+            tf.keras.layers.experimental.preprocessing.StringLookup(
+                vocabulary=unique_movie_titles, mask_token=None),
+            tf.keras.layers.Embedding(len(unique_movie_titles) + 1, embedding_dimension)
+        ])
+
+        self.user_model: tf.keras.layers.Layer = tf.keras.Sequential([
+            tf.keras.layers.experimental.preprocessing.StringLookup(
+                vocabulary=unique_user_ids, mask_token=None),
+            tf.keras.layers.Embedding(len(unique_user_ids) + 1, embedding_dimension)
+        ])
+
+        self.rating_model = tf.keras.Sequential([
+            tf.keras.layers.Dense(256, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(0.01)),
